361056f2dd1c26638c573a2cd876705abc3cfd1a
nehemiahrubio <97257122+nehemiahrubio@users.noreply.github.com>
2024-03-01 16:11:22 -0800

    1

---
+++ client/src/components/auth/UserConfirmRoute.js
@@ -0,0 +1,9 @@
+// UserConfirmRoute.js
+import React from 'react';
+import UserConfirm from "./UserConfirm";
+
+const UserConfirmRoute = (props) => {
+  return <UserConfirm {...props} />;
+};
+
+export default UserConfirmRoute;
